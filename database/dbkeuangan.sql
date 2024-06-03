5a69d5bc8d16bd8ef993704eba3f23123e0da36a
p1xlb <tempakkul@gmail.com>
2024-06-04 07:18:11 +0800

    update bigint for row total on tb_pendapatan

--- database/dbkeuangan.sql
+++ database/dbkeuangan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 03, 2024 at 01:05 AM
+-- Generation Time: Jun 04, 2024 at 01:17 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -43,11 +43,9 @@ CREATE TABLE `tb_alokasi` (
 --
 
 INSERT INTO `tb_alokasi` (`id`, `alokasi`, `kota`, `tahun`, `jumlah`, `deskripsi`, `status`, `catatan`) VALUES
-(1, 'teknologi', 'Kab. Kutai Kartanegara', 2024, 25000, 'pembangunan tower di kecamatan', 'setuju', ''),
-(2, 'pendidikan', 'Kota Samarinda', 2024, 12000000, 'Bantuan peremajaan sekolar', 'revisi', 'kurang'),
-(3, 'teknologi', 'Kab. Berau', 2024, 1200000, 'TECK', 'pending', NULL),
-(4, 'teknologi', 'Kab. Berau', 2024, 5000000000, 'tehc 2', 'setuju', ''),
-(5, 'pendidikan', 'Kab. Berau', 2024, 120000000000, 'dana bos', 'setuju', '');
+(1, 'pendidikan', 'Kab. Berau', 2024, 250000000000, 'Dana BOS', 'setuju', ''),
+(2, 'kesehatan', 'Kab. Berau', 2024, 250000000000, 'Puskesmas', 'setuju', ''),
+(3, 'infrastruktur', 'Kab. Berau', 2024, 250000000000, 'Pengembangan Infrastruktur Beraw', 'setuju', '');
 
 -- --------------------------------------------------------
 
@@ -67,12 +65,7 @@ CREATE TABLE `tb_anggaran` (
 --
 
 INSERT INTO `tb_anggaran` (`id`, `kota`, `tahun`, `total`) VALUES
-(1, 'Kab. Kutai Kartanegara', 2024, 2000000),
-(2, 'Kab. Kutai Timur', 2024, 500000),
-(3, 'Kota Samarinda', 2024, 500000000),
-(4, 'Kab. Mahakam Ulu', 2024, 500000000),
-(6, 'Kab. Berau', 2024, 500000000),
-(8, 'Kota Bontang', 2024, 1000000000);
+(1, 'Kab. Berau', 2024, 1000000000000);
 
 -- --------------------------------------------------------
 
@@ -84,18 +77,10 @@ CREATE TABLE `tb_pendapatan` (
   `id` int(11) NOT NULL,
   `nama_pendapatan` varchar(100) NOT NULL,
   `kota` varchar(100) NOT NULL,
-  `total` int(100) NOT NULL,
+  `total` bigint(100) NOT NULL,
   `tahun` int(100) NOT NULL,
   `deskripsi` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tb_pendapatan`
---
-
-INSERT INTO `tb_pendapatan` (`id`, `nama_pendapatan`, `kota`, `total`, `tahun`, `deskripsi`) VALUES
-(3, 'sawit', 'Kab. Kutai Timur', 2500000, 2024, 'penjualan'),
-(4, 'Persawahan', 'Kota Samarinda', 120000000, 2024, 'Ekspor Padi ke Thailand');
 
 -- --------------------------------------------------------
 
@@ -117,7 +102,10 @@ CREATE TABLE `tb_user` (
 
 INSERT INTO `tb_user` (`id_user`, `nama`, `username`, `password`, `role`) VALUES
 (1, 'admin', 'kepala', '123', 'admin'),
-(2, 'zaid', 'pegawai', '123', 'user');
+(2, 'zaid', 'pegawai', '123', 'user'),
+(3, 'admin', 'admin', '123', 'super'),
+(4, 'Ujang Mahendra', 'umahendra', 'ujang', 'admin'),
+(5, 'Udin Mahendra', 'udinmah', '5510007174', 'user');
 
 --
 -- Indexes for dumped tables
@@ -155,25 +143,25 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT for table `tb_alokasi`
 --
 ALTER TABLE `tb_alokasi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tb_anggaran`
 --
 ALTER TABLE `tb_anggaran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tb_pendapatan`
 --
 ALTER TABLE `tb_pendapatan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
